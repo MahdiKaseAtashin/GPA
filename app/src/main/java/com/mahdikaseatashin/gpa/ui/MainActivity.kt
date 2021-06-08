1cc205a9face9968afb86ecdaeb69ebbd7beb940
Mahdi <mahdikaseatashin@gmail.com>
2021-06-08 14:07:20 +0430

    + Check network connection added!

--- app/src/main/java/com/mahdikaseatashin/gpa/ui/MainActivity.kt
+++ app/src/main/java/com/mahdikaseatashin/gpa/ui/MainActivity.kt
@@ -2,26 +2,27 @@ package com.mahdikaseatashin.gpa.ui
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.mahdikaseatashin.gpa.BuildConfig
 import com.mahdikaseatashin.gpa.adapter.NodesAdapter
 import com.mahdikaseatashin.gpa.adapter.RecyclerItemClickListener
 import com.mahdikaseatashin.gpa.api.RetrofitService
 import com.mahdikaseatashin.gpa.databinding.ActivityMainBinding
 import com.mahdikaseatashin.gpa.repository.MainRepository
+import com.mahdikaseatashin.gpa.utils.ConnectionType
 import com.mahdikaseatashin.gpa.utils.Constants
+import com.mahdikaseatashin.gpa.utils.NetworkMonitorUtil
 import com.mahdikaseatashin.gpa.viewmodel.MainViewModel
 import com.mahdikaseatashin.gpa.viewmodel.MyViewModelFactory
 import timber.log.Timber
 
 
 class MainActivity : AppCompatActivity() {
+    private val networkMonitor = NetworkMonitorUtil(this)
     lateinit var viewModel: MainViewModel
     private val adapter = NodesAdapter()
     lateinit var binding: ActivityMainBinding
@@ -34,7 +35,10 @@ class MainActivity : AppCompatActivity() {
         val mainRepository = MainRepository(retrofitService)
 
         binding.rvReposMain.adapter = adapter
-        viewModel = ViewModelProvider(this, MyViewModelFactory(mainRepository)).get(MainViewModel::class.java)
+        viewModel = ViewModelProvider(
+            this,
+            MyViewModelFactory(mainRepository)
+        ).get(MainViewModel::class.java)
         viewModel.nodeList.observe(this, {
             adapter.setNodes(it)
             binding.rvReposMain.addOnItemTouchListener(
@@ -44,7 +48,7 @@ class MainActivity : AppCompatActivity() {
                     object : RecyclerItemClickListener.OnItemClickListener {
                         override fun onItemClick(view: View?, position: Int) {
                             // do whatever
-                            val intent = Intent(this@MainActivity,DetailsActivity::class.java)
+                            val intent = Intent(this@MainActivity, DetailsActivity::class.java)
                             intent.putExtra(Constants.SELECTED_NODE_KEY, it[position])
                             startActivity(intent)
                         }
@@ -67,7 +71,39 @@ class MainActivity : AppCompatActivity() {
                 binding.pbLoadingMain.visibility = View.GONE
             }
         })
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+        Timber.e("timber test")
+        networkMonitor.result = { isAvailable, type ->
+            when (isAvailable) {
+                true -> {
+                    when (type) {
+                        ConnectionType.Wifi -> {
+                            Timber.e("Wifi Connection")
+                            viewModel.getAllMovies()
+                            networkMonitor.unregister()
+                        }
 
-        viewModel.getAllMovies()
+                        ConnectionType.Cellular -> {
+                            Timber.e("Cellular Connection")
+                            viewModel.getAllMovies()
+                            networkMonitor.unregister()
+                        }
+                        else -> {}
+                    }
+                }
+                false -> {
+                    Toast.makeText(this, "No internet!", Toast.LENGTH_SHORT).show()
+                    Timber.e("No Connection")
+                }
+            }
+        }
     }
+
+    override fun onResume() {
+        super.onResume()
+        networkMonitor.register()
+    }
+
 }
--- app/src/main/java/com/mahdikaseatashin/gpa/utils/Constants.kt
+++ app/src/main/java/com/mahdikaseatashin/gpa/utils/Constants.kt
@@ -1,6 +1,17 @@
 package com.mahdikaseatashin.gpa.utils
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat.getSystemService
+import timber.log.Timber
+
+
 object Constants {
     const val BASE_URL = "https://api.github.com/"
     const val SELECTED_NODE_KEY = "selected node"
+
 }
